3afa978f0cc580042186c554d88b06d99705022d
Christian Maines <christian1006@gmail.com>
2020-04-05 00:30:13 +0200

    the solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+   
+  solverSudoku(matrix);
+
+  function checkValidation(matrix, horizontal, vertical, number) {
+    for (let i = 0; i < 9; i++) {
+      let horizontalBox = 3 * Math.floor(horizontal / 3) + Math.floor(i / 3); 
+      let verticalBox = 3 * Math.floor(vertical / 3) + i % 3;
+      if (number == matrix[horizontal][i] || number == matrix[i][vertical] || number == matrix[horizontalBox][verticalBox]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function solverSudoku(matrix) {
+    for (let horizontal = 0; horizontal < 9; horizontal++) {
+      for (let vertical = 0; vertical < 9; vertical++){
+        if (matrix[horizontal][vertical] === 0) {
+        for (let number = 1; number < 10; number++) {
+          if (checkValidation(matrix, horizontal, vertical, number)) {
+           matrix[horizontal][vertical] = number;
+             if (solverSudoku(matrix)) {
+              return true;
+             }  matrix[horizontal][vertical] = 0;
+          }
+        }
+        return false;
+        }
+      }
+    }
+    return true;
+  }
+  return matrix;
 }
